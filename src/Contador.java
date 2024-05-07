30dc35fb29c8cfcc43ba26db263b551fe3d35a5b
Ribeiro <matheus22.matias@gmail.com>
2024-05-07 20:54:53 -0300

    first commit

---
+++ src/Contador.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+
+        Scanner entr = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro parametro: ");
+        int parametroUm = entr.nextInt();
+
+        System.out.println("Digite o segundo parametro: ");
+        int parametroDois = entr.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametroInvalidosExcepetion e) {
+            
+        }
+
+    }
+
+
+    public static void contar(int parametroUm, int parametroDois) throws ParametroInvalidosExcepetion {
+        //Aqui é validado se o 1° parametro é maior que o segundo. Sendo maior ele gera uma exceção baseada na regra
+        if (parametroUm > parametroDois) {
+            throw new ParametroInvalidosExcepetion();
+        }else{
+            //Se não, ele faz a contagem dos números do parametroUm até o parametroDois
+             int contagem = parametroDois - parametroUm;
+
+             for (int i = 0; i < contagem; i++) {
+                System.out.println("Imprimindo o número " + (i + (parametroUm+1)));
+            }
+        }
+
+
+        
+    }
+
+}
---
+++ src/ParametroInvalidosExcepetion.java
@@ -0,0 +1,8 @@
+
+public class ParametroInvalidosExcepetion extends Exception {
+    
+    public ParametroInvalidosExcepetion(){
+        System.err.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+
+}
